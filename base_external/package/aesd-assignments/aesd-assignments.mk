97b0bcfbca8a2ff49da6b36355069cc9a3187366
pravinraghul <pravinraghul@gmail.com>
2024-06-26 09:00:28 +0530

    Completed assignment 8

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -2,17 +2,19 @@
 ##############################################################
 #
 # AESD-ASSIGNMENTS
-#
-##############################################################
+###############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '6e82e8a2ec4b2b99dc6dfba52bd0fda03172c663'
+AESD_ASSIGNMENTS_VERSION = '2af7f00ddb8bf41b8fb5e4503d8ae2887b1a5d39'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
 AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-pravinraghul.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
+
+# Adding the aesd-char-driver module
+AESD_ASSIGNMENTS_MODULE_SUBDIRS += aesd-char-driver
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
@@ -30,4 +32,5 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop.sh $(TARGET_DIR)/etc/init.d/S99aesdsocket
 endef
 
+$(eval $(kernel-module))
 $(eval $(generic-package))
